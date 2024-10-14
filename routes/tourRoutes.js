beeb367b5afc8c755fd92443de3707693a6d2785
clivissonjose <cjbarbosa2001@gmail.com>
2024-10-13 22:12:14 -0300

    feat: using aggragate pipeline to see tours by month and a year

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,6 +4,12 @@ const router = express.Router();
 
 // router.param("id", tourController.checkID);
 
+router.route("/tours-stats")
+  .get(tourController.getTourStats);
+
+router.route("/tours-monthlyPlan:year")
+  .get(tourController.getMonthlyPlan);
+
 router.route("/top-5-cheap")
   .get(tourController.aliasTopTours, tourController.getAllTours);
   
